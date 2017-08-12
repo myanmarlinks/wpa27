59744a567dc55b37af8247705de0d1711e12b5c8
phpcrazy <phpcrazy@gmail.com>
2017-08-12 18:37:15 +0630

    URI Request

--- phpframe/public/index.php
+++ phpframe/public/index.php
@@ -5,10 +5,24 @@ define("DD", realpath("../"));
 require DD . "/wpa27/functions.php";
 require  DD . "/app/controller/controllers.php";
 
-if(isset($_GET['page'])) {
-	$page = $_GET['page'];
-} else {
+$request_uri = explode("/", $_SERVER['REQUEST_URI']);
+$script_name = explode("/", $_SERVER['SCRIPT_NAME']);
+
+$uri = array_diff($request_uri, $script_name);
+$a_uri = array_values($uri);
+// var_dump($a_uri);
+// die();
+
+// if(isset($_GET['page'])) {
+// 	$page = $_GET['page'];
+// } else {
+// 	$page = "home";
+// }
+
+if(empty($a_uri)) {
 	$page = "home";
+} else {
+	$page = $a_uri[0];
 }
 
 $controller = ucfirst($page) . "Controller";
