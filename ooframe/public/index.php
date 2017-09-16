bcd606e310206f1065a606579b18b2aa61700844
phpcrazy <phpcrazy@gmail.com>
2017-09-16 18:00:26 +0630

    Singleton Pattern

--- ooframe/public/index.php
+++ ooframe/public/index.php
@@ -8,13 +8,27 @@ $students = DB::table("students")->get(); // method chain
 $classes = DB::table("classes")->get();
 $products = DB::table("products")->get();
 
-class DB {
+class DB extends PDO {
 
-	private static $_instance;
+	private $engine; 
+    private $host; 
+    private $database; 
+    private $user; 
+    private $pass; 
 
-	public function __construct() {
-		echo "Constructor! <br>";
-	}
+    private static $_instance;
+    
+    public function __construct(){ 
+        $this->engine = 'mysql'; 
+        $this->host = 'localhost'; 
+        $this->database = 'wpa27'; 
+        $this->user = 'root'; 
+        $this->pass = ''; 
+        $dns = $this->engine.':dbname='.$this->database.";host=".$this->host; 
+        parent::__construct( $dns, $this->user, $this->pass ); 
+    } 
+
+	
 
 	public function __destruct() {
 		echo "Destructor! <br>";
@@ -28,7 +42,7 @@ class DB {
 	}
 
 	public function get() {
-		echo "GETTER <br>";
+		var_dump($this);
 	}
 
 
