ef3969948178e28c907d5f7a7f3edb627852bdce
phpcrazy <phpcrazy@gmail.com>
2017-09-16 18:47:20 +0630

    Singleton Database

--- ooframe/public/index.php
+++ ooframe/public/index.php
@@ -5,8 +5,10 @@ define("DD", realpath("../"));
 // Singleton Pattern
 
 $students = DB::table("students")->get(); // method chain
+$student  = DB::table("students")->where(["id"=> 1])->get();
 $users = DB::table("users")->get();
 var_dump($students);
+var_dump($student);
 var_dump($users);
 // $classes = DB::table("classes")->get();
 // $products = DB::table("products")->get();
@@ -14,24 +16,27 @@ var_dump($users);
 class DB extends PDO {
 
 	private $engine; 
-    private $host; 
-    private $database; 
-    private $user; 
-    private $pass; 
+	private $host; 
+	private $database; 
+	private $user; 
+	private $pass; 
 
-    private static $_instance;
-    private $_table_name;
-    
-    public function __construct(){ 
-    	echo "DB Object Constructed<br>";
-        $this->engine = 'mysql'; 
-        $this->host = 'localhost'; 
-        $this->database = 'wpa27'; 
-        $this->user = 'root'; 
-        $this->pass = ''; 
-        $dns = $this->engine.':dbname='.$this->database.";host=".$this->host; 
-        parent::__construct( $dns, $this->user, $this->pass ); 
-    }
+	private static $_instance;
+	private $table_name;
+	private $where_statement = null;
+	private $where_value = null;
+	private $where_trigger = false;
+
+	public function __construct(){ 
+		echo "DB Object Constructed<br>";
+		$this->engine = 'mysql'; 
+		$this->host = 'localhost'; 
+		$this->database = 'wpa27'; 
+		$this->user = 'root'; 
+		$this->pass = ''; 
+		$dns = $this->engine.':dbname='.$this->database.";host=".$this->host; 
+		parent::__construct( $dns, $this->user, $this->pass ); 
+	}
 
 	public function __destruct() {
 		echo "Destructor! <br>";
@@ -41,19 +46,42 @@ class DB extends PDO {
 		if(!self::$_instance instanceof DB) {
 			self::$_instance = new DB();
 		}
-		self::$_instance->_table_name = $table_name;
+		self::$_instance->table_name = $table_name;
 		return self::$_instance;
 	}
 
-	public function get() {
-		$sql = "SELECT * FROM " . $this->_table_name;
-		$prep = $this->prepare($sql);
-		$prep->execute();
+	public function where(array $where_data) {
+		$this->where_trigger = true;
+		$where_statement = " WHERE ";
+		foreach ($where_data as $key => $value) {
+			$where_statement .= $key . " = :" . $key . " AND ";
+		}
+		$result_statement = substr($where_statement, 0, -4);
+		$this->where_statement = $result_statement;
+		$this->where_value = $where_data;
+		return $this;
+	}
 
-		return $prep->fetchAll(PDO::FETCH_ASSOC);
+	public function get() {
+		if($this->where_trigger == false) {
+			$sql = "SELECT * FROM " . $this->table_name;
+			$prep = $this->prepare($sql);
+			$prep->execute();
+			return $prep->fetchAll(PDO::FETCH_ASSOC);	
+		} else {
+
+			$sql = "SELECT * FROM " . $this->table_name . $this->where_statement;
+			$prep = $this->prepare($sql);
+			$prep->execute($this->where_value);
+			$this->where_statement = null;
+			$this->where_value = null;
+			$this->where_trigger = false;
+			return $prep->fetchAll(PDO::FETCH_ASSOC);	
+		}
+		
 	}
 
 
 }
 
- ?>
+?>
